b76497efc86f37dcc7bfa5715a2ccfe11995603c
ppisanchyn <ppisanchyn@gmail.com>
2024-12-31 07:09:51 +0500

    Add files via upload

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,9 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('', views.index),
+    path('about/', views.about),
+    path('services/', views.services),
+    path('products/', views.products),
+    path('faq/', views.faq),
+    path('contacts/', views.contacts),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+
+def about(request):
+    return render(request,'about.html')
+
+def services(request):
+    return render(request,'services.html')
+def products(request):
+    return render(request,'products.html')
+def faq(request):
+    return render(request,'faq.html')
+def contacts(request):
+    return render(request,'contact.html')
+
+
+
+
+
+
+
